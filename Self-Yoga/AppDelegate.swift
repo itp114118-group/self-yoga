4aaf30e4747d233918fdea0f7691360d13dcd5f1
Lee Chak Kei <63443381+lchakkei@users.noreply.github.com>
2020-12-08 19:29:13 +0800

    Link to Firebase

--- Self-Yoga/AppDelegate.swift
+++ Self-Yoga/AppDelegate.swift
@@ -2,11 +2,12 @@
 //  AppDelegate.swift
 //  Self-Yoga
 //
-//  Created by itst on 25/11/2020.
+//  Created by Jack on 8/12/2020.
 //
 
 import UIKit
 import CoreData
+import Firebase
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,6 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        FirebaseApp.configure()
         return true
     }
 
